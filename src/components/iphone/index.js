09733802669058448b7ac06fe27d53c9b05a325f
Harvey Randall <harvey_randall@me.com>
2019-02-14 16:50:40 +0000

    google autocomplete works

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -5,16 +5,39 @@ import { Router, Route, Link } from 'preact-router';
 import Home from "../home";
 import Search from '../search';
 
+import $ from 'jquery';
+
 export default class Iphone extends Component {
 	constructor() {
 		super();
 	}
 
+	componentDidMount() {
+		this.fetchLocation();
+	}
+
+	fetchLocation() {
+		$.ajax({
+			url: "https://extreme-ip-lookup.com/json",
+			dataType: "jsonp",
+			success: (data) => {
+				this.setState({
+					city: data.city
+				})
+			},
+			error: (res, err) => {
+				alert(err);
+			}
+		});
+	}
+
+
+
 	render() {
 		return (
 			<Router>
 				<Route path="/" component={Home} />
-				<Route path="/search" component={() => <Search location="London" />} />
+				<Route path="/search" component={() => <Search location={this.state.city} />} />
 			</Router>
 		);
 	}
--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,10 +1,13 @@
 // import preact
 import { h, render, Component } from 'preact';
 import { Link } from 'preact-router';
+
+const $ = window.$;
+
 // import stylesheets for ipad & button
 import style from './style';
 // import jquery for API calls
-import $ from 'jquery';
+//import $ from 'jquery';
 // import the Button component
 import Button from '../button';
 import style_iphone from '../button/style_iphone';
@@ -26,24 +29,62 @@ export default class Search extends Component {
 	render() {
 		return (
 			<div class={style.container}>
-				<Navigation query={this.state.searchQuery}/>
+				<Navigation query={this.props.location}/>
 				<Results />
 			</div>
 		);
 	};
 }
 
-const Navigation = (props) => {
-	return (
-		<div class={style.navigation}>
-			<form>
-				<input type="text" class="form-control" value={props.query} placeholder="Search..." />
-			</form>
-			<Link className={style.back} href={"/"}>
-				<i class="fas fa-arrow-right"></i>
-			</Link>
-		</div>
-	);
+class Navigation extends Component {
+	constructor(props) {
+		super(props);
+
+    	this.autocomplete = null;
+    	this.handlePlaceChanged = this.handlePlaceChanged.bind(this);
+
+	}
+
+	componentDidMount() {
+		//this.findLocation(this.props.query);
+		this.autocompleteInput = document['place-search'].place;
+		this.autocomplete = new google.maps.places.Autocomplete(this.autocompleteInput,
+        {"types": ["geocode"], "radius": 500});
+
+    	this.autocomplete.addListener('place_changed', this.handlePlaceChanged);
+	}
+
+	handlePlaceChanged() {
+		const place = this.autocomplete.getPlace();
+		alert(place);
+	}
+
+	findLocation(city) {
+		$.ajax({
+			url: `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${city}&types=geocode&radius=500&key=${config.google_maps_key}`,
+			dataType: "jsonp",
+			success: (data) => {
+				console.log(data);
+			}
+		});
+	}
+
+	endOfTyping(event) {
+		this.timer=setTimeout(this.findLocation, 3000)
+	}
+
+	render() {
+		return (
+			<div class={style.navigation}>
+				<form name="place-search">
+					<input type="text" class="form-control" name="place" value={this.props.query} placeholder="Search..." />
+				</form>
+				<Link className={style.back} href={"/"}>
+					<i class="fas fa-arrow-right"></i>
+				</Link>
+			</div>
+		);
+	}
 }
 
 const Results = (props) => {
